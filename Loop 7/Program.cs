e1c8a634c3dc91677b5604b240026fe666896181
nehy06001 <nehy06001@E-PW05CGQV>
2024-12-19 15:06:02 +0100

    Add project files.

---
+++ Loop 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Skriv in ett positivt tal: ");
+        int nummer = int.Parse(Console.ReadLine());
+
+        while (nummer >= 0)
+        {
+            Console.Write("Skriv in ett positivt tal: ");
+            nummer = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Du skrev in ett negativt tal. Programmet avslutas.");
+    }
+}
